63cae9b91642b786adcc57f30b7a5feec7612250
im-24 <ikrmmhmdi@gmail.com>
2026-03-29 08:46:02 +0100

    test src for server and node

---
+++ PlatformIO/conx_nodemcu-server3/src/node/node.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <WifiEspNow.h>
+
+void setup() {
+    delay(20);
+    Serial.begin(115200);
+    Serial.println("the node  is here ");
+}
+
+void loop() {
+  delay(1000);
+}
